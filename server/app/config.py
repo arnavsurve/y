ee34d6d2f573c7c9f9a6c3cb6cf57dffbba7a672
Arnav Surve <arnav@surve.dev>
2025-03-03 12:41:51 -0800

    groq!

--- server/app/config.py
+++ server/app/config.py
@@ -7,6 +7,7 @@ load_dotenv()
 class Settings(BaseSettings):
     app_port: int
     chroma_port: int
+    groq_api_key: str
 
     model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
 
